05ef7e55942b3af16a55cb498ce649e917dd9be9
John Lindquist <johnlindquist@gmail.com>
2019-05-16 17:35:40 -0600

    Thu May 16 17:35:40 MDT 2019

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config()
+
+const Nightmare = require("nightmare")
+const nightmare = Nightmare({ show: true })
+const download = require("download")
+const fs = require("fs")
+
+//https://discordapp.com/channels/@me/539924277881995292
+
+const loginSelector = `#app-mount > div.app-19_DXt.platform-web > div > div.leftSplit-1qOwnR.hasLogo-2bq2VW > div > form > div > div.block-egJnc0.marginTop20-3TxNs6 > div.marginBottom20-32qID7 > div > input`
+const passwordSelector = `#app-mount > div.app-19_DXt.platform-web > div > div.leftSplit-1qOwnR.hasLogo-2bq2VW > div > form > div > div.block-egJnc0.marginTop20-3TxNs6 > div:nth-child(2) > div > input`
+const buttonSelector = `#app-mount > div.app-19_DXt.platform-web > div > div.leftSplit-1qOwnR.hasLogo-2bq2VW > div > form > div > div.block-egJnc0.marginTop20-3TxNs6 > button.marginBottom8-AtZOdT.button-3k0cO7.button-38aScr.lookFilled-1Gx00P.colorBrand-3pXr91.sizeLarge-1vSeWK.fullWidth-1orjjo.grow-q77ONN`
+
+nightmare
+  .goto("https://discordapp.com/channels/@me/539924277881995292")
+  .type(loginSelector, process.env.EMAIL)
+  .type(passwordSelector, process.env.PASSWORD)
+  .click(buttonSelector)
+  .wait(5000)
+  .goto("https://discordapp.com/channels/@me/539924277881995292")
+  .wait("a[href^='https://www.gifyourgame.com']")
+  .evaluate(() =>
+    Array.from(
+      document.querySelectorAll("a[href^='https://www.gifyourgame.com']")
+    )
+      .map(e => e.innerText)
+      .filter(text => text)
+      .map(link => `${link}_1080p.mp4`)
+  )
+  .end()
+  .then(async results => {
+    for (let videoUrl of results) {
+      await download(videoUrl, "video")
+    }
+  })
+  .catch(error => {
+    console.error("Search failed:", error)
+  })
